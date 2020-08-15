620aa7e8c024e9f1472897a6b23578ba21f55726
songmeo <shuong1012@gmail.com>
2020-08-15 21:23:53 +0300

    search bar

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="entry")
+    path("<str:title>", views.entry, name="entry"),
+    path("search/", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -13,3 +13,17 @@ def entry(request, title):
         "entry": util.get_entry(title),
         "title": title
     })
+
+def search(request):
+    title = request.GET.get('q')
+    entries = util.list_entries()
+    if title in entries:
+        return render(request, "encyclopedia/entry.html", {
+        "entry": util.get_entry(title),
+        "title": title
+    })
+    else:
+        results = [e for e in entries if title in e]
+        return render(request, "encyclopedia/search_result.html", {
+            "results":  results
+        })
